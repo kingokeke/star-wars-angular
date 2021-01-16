027c1c2379a20a6e181f4927ac7f04142449d994
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-16 15:42:53 +0100

    Routed click of logo to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +10,7 @@ export class DashboardComponent implements OnInit {
   logoUrl = 'assets/images/logo.png';
   userImageUrl = 'assets/images/user.png';
   navLinks = [
-    { name: 'Dashboard', url: '', icon: 'tachometer-alt' },
+    { name: 'Dashboard', url: '/dashboard', icon: 'tachometer-alt' },
     { name: 'Starships', url: '/starships', icon: 'rocket' },
     { name: 'People', url: '/people', icon: 'user-friends' },
     { name: 'Vehicles', url: '/vehicles', icon: 'truck-monster' },
@@ -17,12 +18,16 @@ export class DashboardComponent implements OnInit {
   ];
   showYearDropdown = false;
 
-  constructor() { }
+  constructor(private readonly router: Router) { }
 
   ngOnInit(): void {
   }
 
   toggleYearDropdown(): void {
     this.showYearDropdown = !this.showYearDropdown;
+  }
+
+  navigateToDashboard(): void {
+    this.router.navigate(['dashboard']);
   }
 }
