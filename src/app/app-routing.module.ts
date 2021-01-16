bbb4485530334da5d41824fa62e3b020ed11e9ee
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-16 05:12:13 +0100

    Added routing for dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
@@ -8,7 +9,7 @@ const routes: Routes = [
   },
   {
     path: 'dashboard', children: [
-      { path: '', component: LoginComponent }
+      { path: '', component: DashboardComponent }
     ]
   }
 ];
