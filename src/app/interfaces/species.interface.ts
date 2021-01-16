9f795f978e6918cbb3df73156b73100e957bb5c9
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 00:46:40 +0100

    Added logic to handle species component data

---
+++ src/app/interfaces/species.interface.ts
@@ -0,0 +1,11 @@
+export interface Species {
+  name: string;
+  classification: string;
+  designation: string;
+  average_height: string;
+  hair_colors: string;
+  eye_colors: string;
+  skin_colors: string;
+  average_lifespan: string;
+  language: string;
+}
--- src/app/species/species.component.ts
+++ src/app/species/species.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Species } from '../interfaces/species.interface';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-species',
   templateUrl: './species.component.html',
   styleUrls: ['./species.component.scss']
 })
-export class SpeciesComponent implements OnInit {
+export class SpeciesComponent implements OnInit, OnDestroy {
+  species: Species[] = [];
 
-  constructor() { }
+  constructor(private readonly dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getData().subscribe(
+      response => this.dataService.setData(response),
+      error => console.log(error)
+    );
+
+    this.dataService.getItems().subscribe(
+      response => {
+        this.species = response;
+        this.dataService.viewLoaded();
+      },
+      error => console.log(error)
+    );
   }
 
+  ngOnDestroy(): void {
+    this.species = [];
+    this.dataService.viewUnloaded();
+  }
+
+  isNumber(item: string): boolean {
+    return !!parseInt(item, 10);
+  }
 }
