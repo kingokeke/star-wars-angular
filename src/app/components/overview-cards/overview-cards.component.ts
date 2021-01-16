6748d37455e289ae0fbc024cb46b1361f0e1e847
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-16 15:47:26 +0100

    Added overview cards component

---
+++ src/app/components/overview-cards/overview-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overview-cards',
+  templateUrl: './overview-cards.component.html',
+  styleUrls: ['./overview-cards.component.scss']
+})
+export class OverviewCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
