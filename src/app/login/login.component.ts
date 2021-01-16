3f84d5bf85420736c620ead5ae6f60f35c6cc58c
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-16 05:11:33 +0100

    Added logic for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  rocketUrl = 'assets/images/rocket.svg';
+  moonUrl = 'assets/images/moon.svg';
+  currentYear = new Date().getFullYear();
+  languages = ['English', 'French', 'Spanish'];
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor(private readonly router: Router) { }
 
   ngOnInit(): void {
   }
 
+  navigateToDashboard(): void {
+    this.router.navigate(['dashboard']);
+  }
+
+  navigateToPrivacyPage(): void {
+  }
+
+  navigateToTermsAndConditionsPage(): void {
+  }
 }
