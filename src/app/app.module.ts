6f4529d11183a46c1e41c14f4812182e92366820
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 01:36:35 +0100

    Moved contents of dashboard module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
-import { DashboardModule } from './dashboard/dashboard.module';
+import { DashboardOverviewComponent } from './components/dashboard-overview/dashboard-overview.component';
+import { NavLinkComponent } from './components/nav-link/nav-link.component';
+import { OverviewCardsComponent } from './components/overview-cards/overview-cards.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FilmsComponent } from './films/films.component';
+import { SpeciesComponent } from './species/species.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { DashboardRoutingModule } from './dashboard/dashboard-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavLinkComponent,
+    FilmsComponent,
+    OverviewCardsComponent,
+    DashboardOverviewComponent,
+    SpeciesComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LoginModule,
-    DashboardModule
+    CommonModule,
+    HttpClientModule,
+    DashboardRoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
