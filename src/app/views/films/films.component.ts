d1e90e7583bfaa26841d04e767d95f3d1f11198c
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 18:23:20 +0100

    Added logic for film component

--- src/app/views/films/films.component.ts
+++ src/app/views/films/films.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostBinding } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+import { Film } from 'src/app/interfaces/film.interface';
 
 @Component({
   selector: 'app-films',
   templateUrl: './films.component.html',
   styleUrls: ['./films.component.scss']
 })
-export class FilmsComponent implements OnInit {
+export class FilmsComponent implements OnInit, OnDestroy {
+  @HostBinding('class') class = 'col';
 
-  constructor() { }
+  films: Film[] = [];
+  filmLogoUrl = 'assets/images/film.svg';
+  demoDirectorUrl = 'assets/images/person.jpg';
+  demoProducerUrl = 'assets/images/person.jpg';
+
+  constructor(private readonly dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getData().subscribe(
+      response => this.dataService.setData(response),
+      error => console.log(error)
+    );
+
+    this.dataService.getItems().subscribe(
+      response => {
+        this.films = response;
+        this.dataService.viewLoaded();
+      },
+      error => console.log(error)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.films = [];
+    this.dataService.viewUnloaded();
+  }
+
+  isNumber(item: string): boolean {
+    return !!parseInt(item, 10);
   }
 
 }
