72204b599a425566df1b73633fb1ddedc0fa2212
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 03:44:25 +0100

    Added logic for people component

--- src/app/views/people/people.component.ts
+++ src/app/views/people/people.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+import { Person } from 'src/app/interfaces/person.interface';
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
   styleUrls: ['./people.component.scss']
 })
-export class PeopleComponent implements OnInit {
+export class PeopleComponent implements OnInit, OnDestroy {
+  people: Person[] = [];
+  demoPersonUrl = 'assets/images/person.jpg';
 
-  constructor() { }
+  constructor(private readonly dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getData().subscribe(
+      response => this.dataService.setData(response),
+      error => console.log(error)
+    );
+
+    this.dataService.getItems().subscribe(
+      response => {
+        this.people = response;
+        this.dataService.viewLoaded();
+      },
+      error => console.log(error)
+    );
   }
+
+  ngOnDestroy(): void {
+    this.people = [];
+    this.dataService.viewUnloaded();
+  }
+
+  isNumber(item: string): boolean {
+    return !!parseInt(item, 10);
+  }
+
 
 }
