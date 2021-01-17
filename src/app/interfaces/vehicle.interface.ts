46748f5acbe8b0e1cbb312b6ae7b12780ffc7e02
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 02:38:15 +0100

    Added interface for vehicles

---
+++ src/app/interfaces/vehicle.interface.ts
@@ -0,0 +1,12 @@
+export interface Vehicle {
+  'name': string;
+  'model': string;
+  'manufacturer': string;
+  'length': string;
+  'max_atmosphering_speed': string;
+  'crew': string;
+  'passengers': string;
+  'cargo_capacity': string;
+  'consumables': string;
+  'vehicle_class': string;
+}
