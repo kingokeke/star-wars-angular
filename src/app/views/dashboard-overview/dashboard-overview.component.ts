a12a19015e073f10e073b7433f84881c8c91102a
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 18:20:49 +0100

    Added data for overview cards

--- src/app/views/dashboard-overview/dashboard-overview.component.ts
+++ src/app/views/dashboard-overview/dashboard-overview.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { SummaryCard } from 'src/app/interfaces/summarycard.interface';
 
 @Component({
   selector: 'app-dashboard-overview',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-overview.component.scss']
 })
 export class DashboardOverviewComponent implements OnInit {
+  @HostBinding('class') class = 'col';
+  cards: SummaryCard[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.cards = [
+      {
+        title: 'Films', totalCount: 17, increaseCount: 1, imageUrl: 'assets/images/film.svg', backgroundColor: 'hsla(171, 97%, 42%, 0.2)'
+      },
+      {
+        title: 'Starships', totalCount: 57, increaseCount: 5, imageUrl: 'assets/images/starship.svg', backgroundColor: 'hsla(219, 100%, 56%, 0.2)'
+      },
+      {
+        title: 'People', totalCount: 14394, increaseCount: 429, imageUrl: 'assets/images/people.svg', backgroundColor: 'hsla(243, 49%, 38%, 0.2)'
+      },
+      {
+        title: 'Vehicles', totalCount: 329, increaseCount: 17, imageUrl: 'assets/images/vehicle.svg', backgroundColor: 'hsla(52, 86%, 59%, 0.2)'
+      },
+      {
+        title: 'Species', totalCount: 3058, increaseCount: 10, imageUrl: 'assets/images/species.svg', backgroundColor: 'hsla(245, 56%, 58%, 0.2)'
+      },
+    ];
   }
 
 }
