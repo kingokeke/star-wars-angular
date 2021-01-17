0129279994b1f7ff637145abb5d456c7002836ca
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 01:36:11 +0100

    Moved routing for dashboard child components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FilmsComponent } from './films/films.component';
+import { SpeciesComponent } from './species/species.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      { path: '', component: FilmsComponent },
+      { path: 'starships', component: LoginComponent },
+      { path: 'people', component: FilmsComponent },
+      { path: 'vehicles', component: LoginComponent },
+      { path: 'species', component: SpeciesComponent },
+      { path: '**', redirectTo: 'dashboard' }
+    ]
+  },
 ];
 
 @NgModule({
