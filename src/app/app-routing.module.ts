7e0d96d952abd37104b25afdf4e89bbde7af55a1
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 18:11:42 +0100

    Updated routing on app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FilmsComponent } from './films/films.component';
-import { SpeciesComponent } from './species/species.component';
+import { DashboardOverviewComponent } from './views/dashboard-overview/dashboard-overview.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { LoginComponent } from './views/login/login.component';
+import { PeopleComponent } from './views/people/people.component';
+import { SpeciesComponent } from './views/species/species.component';
+import { StarshipsComponent } from './views/starships/starships.component';
+import { VehiclesComponent } from './views/vehicles/vehicles.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, children: [{ path: '', component: FilmsComponent }] },
-  { path: 'starships', component: DashboardComponent, children: [{ path: '', component: FilmsComponent }] },
-  { path: 'people', component: DashboardComponent, children: [{ path: '', component: FilmsComponent }] },
-  { path: 'vehicles', component: DashboardComponent, children: [{ path: '', component: FilmsComponent }] },
+  { path: 'dashboard', component: DashboardComponent, children: [{ path: '', component: DashboardOverviewComponent }] },
+  { path: 'starships', component: DashboardComponent, children: [{ path: '', component: StarshipsComponent }] },
+  { path: 'people', component: DashboardComponent, children: [{ path: '', component: PeopleComponent }] },
+  { path: 'vehicles', component: DashboardComponent, children: [{ path: '', component: VehiclesComponent }] },
   { path: 'species', component: DashboardComponent, children: [{ path: '', component: SpeciesComponent }] },
   { path: '**', redirectTo: '' }
 ];
