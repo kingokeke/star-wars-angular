3202e3f0ba87ab6f14694b3ed783600c2d63bbd8
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 02:36:45 +0100

    Added logic for vehicles component

--- src/app/views/vehicles/vehicles.component.ts
+++ src/app/views/vehicles/vehicles.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+import { Vehicle } from 'src/app/interfaces/vehicle.interface';
 
 @Component({
   selector: 'app-vehicles',
   templateUrl: './vehicles.component.html',
   styleUrls: ['./vehicles.component.scss']
 })
-export class VehiclesComponent implements OnInit {
+export class VehiclesComponent implements OnInit, OnDestroy {
+  vehicles: Vehicle[] = [];
 
-  constructor() { }
+  constructor(private readonly dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.getData().subscribe(
+      response => this.dataService.setData(response),
+      error => console.log(error)
+    );
+
+    this.dataService.getItems().subscribe(
+      response => {
+        this.vehicles = response;
+        this.dataService.viewLoaded();
+      },
+      error => console.log(error)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.vehicles = [];
+    this.dataService.viewUnloaded();
+  }
+
+  isNumber(item: string): boolean {
+    return !!parseInt(item, 10);
   }
 
 }
