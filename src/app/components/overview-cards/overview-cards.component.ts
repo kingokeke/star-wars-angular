6cf4ee9d4bf23192c9e285d380be1e0b974002da
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2021-01-17 18:17:31 +0100

    Added logic for populating overview card

--- src/app/components/overview-cards/overview-cards.component.ts
+++ src/app/components/overview-cards/overview-cards.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { SummaryCard } from '../../interfaces/summarycard.interface';
 
 @Component({
   selector: 'app-overview-cards',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview-cards.component.scss']
 })
 export class OverviewCardsComponent implements OnInit {
+  @HostBinding('class') class = 'col';
+  @Input() cardData: SummaryCard = {
+    title: 'Loading...',
+    totalCount: 0,
+    increaseCount: 0,
+    imageUrl: 'assets/images/dummy.svg',
+    backgroundColor: 'hsla(0, 10%, 10%, 0.2)'
+  };
 
   constructor() { }
 
